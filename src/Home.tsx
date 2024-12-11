44550d712cce2ec69ca42ab25a2e4a5a40617559
Bryan Gordon <bryanbge92@gmail.com>
2024-12-10 19:41:41 -0500

    Refactor component

--- src/Home.tsx
+++ src/Home.tsx
@@ -8,34 +8,11 @@ export function Home () {
   const [degrees, setDegrees] = useState<number>(0)
   const [result, setResult] = useState(0)
 
-  const convertDegrees = (degrees: number) => {
-    if (!degrees) {
-      window.alert('Error al calcular.')
-    } else {
-      let arrValue: string[] = ['']
-      let converNum: number = 0
-      const value = Number((1.8 * degrees + 32).toFixed())
-      arrValue = value.toString().split('')
-      const last = Number(arrValue[arrValue.length - 1])
+  setResult(0/*Colocar funcion*/)
 
-      if (last > 0 && last <= 4) {
-        arrValue[arrValue.length - 1] = '0'
-        converNum = Number(arrValue.join(''))
-        setResult(converNum)
-      } else if (last >= 7) {
-        arrValue[arrValue.length - 1] = '0'
-        converNum = Number(arrValue.join(''))
-        converNum += 10
-        setResult(converNum)
-      } else if (last === 6) {
-        arrValue[arrValue.length - 1] = '5'
-        converNum = Number(arrValue.join(''))
-        setResult(converNum)
-      } else {
-        setResult(value)
-      }
-    }
-  }
+ /* const convertDegrees = (degrees: number) => {
+    
+  }*/
 
   return (
     <main>
